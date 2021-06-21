1c5ac55d2e98892c1d43058656d2c273aa85f7be
PeHaa <info@pehaa.com>
2021-06-21 10:40:07 +0200

    install provider footer and features

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,6 +2,7 @@ import docs from "../assets/docs.svg"
 import coffee from "../assets/coffee.svg"
 import screen from "../assets/screen.svg"
 import Feature from "./Feature"
+import { Container, SimpleGrid } from "@chakra-ui/react"
 
 const Features = () => {
   const features = [
@@ -22,11 +23,13 @@ const Features = () => {
     },
   ]
   return (
-    <section>
-      {features.map((el, index) => {
-        return <Feature key={index} {...el}></Feature>
-      })}
-    </section>
+    <Container as="section" maxW="container.lg" py="10">
+      <SimpleGrid columns={[1, null, 3]} gap="8">
+        {features.map((el, index) => {
+          return <Feature key={index} {...el}></Feature>
+        })}
+      </SimpleGrid>
+    </Container>
   )
 }
 
